3d58c152af7c2a15dacd3754e9c3b1aa5d10d8a2
vrra <vishnu.n@ieee.org>
2023-05-16 11:56:58 +0530

    minor changes

--- PythonScripts2023/PushRecordToArgilla-HF.py
+++ PythonScripts2023/PushRecordToArgilla-HF.py
@@ -1,5 +1,5 @@
 import os
+print(os.environ.get("API_URL"))
 print(os.environ.get("API_KEY"))
 print(os.environ.get("ARGILLA_WORKSPACE"))
-print(os.environ.get("MY_GITHUB_TOKEN"))
 print(os.environ.get("VISHNURAMOV_PUSH_TO_HF_HUB_TOKEN"))

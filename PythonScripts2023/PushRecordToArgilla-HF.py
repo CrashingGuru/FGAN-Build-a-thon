13c58a31d0c4364c4f8d8604cf996f9b455d620f
vrra <vishnu.n@ieee.org>
2023-05-16 16:18:44 +0530

    minor changes

--- PythonScripts2023/PushRecordToArgilla-HF.py
+++ PythonScripts2023/PushRecordToArgilla-HF.py
@@ -17,6 +17,13 @@ import subprocess
 import sys
 
 subprocess.check_call([sys.executable, "-m", "pip", "install", 'argilla'])
+# process output with an API in the subprocess module:
+reqs = subprocess.check_output([sys.executable, '-m', 'pip',
+'freeze'])
+installed_packages = [r.decode().split('==')[0] for r in reqs.split()]
+
+print(installed_packages)
+
 import argilla as rg
 
 uri= os.environ.get("API_URL")
